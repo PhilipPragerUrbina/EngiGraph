0d63bff322287004cbb8c85a9b23a307d2327095
PhilipPragerUrbina <72355251+PhilipPragerUrbina@users.noreply.github.com>
2023-09-29 20:15:00 -0700

    Basic error popup.

--- src/main.cpp
+++ src/main.cpp
@@ -40,18 +40,6 @@ int main(int, char**)
         return 1;
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1); // Enable vsync
-    EngiGraph::initOpenGl((GLADloadproc)glfwGetProcAddress);
-    EngiGraph::Camera camera{EngiGraph::AngleDegrees<float>(90.0f),1.0f,0.001,1000,{0,-1,0}};
-    camera.setPosition({5.0,5.0,5.0});
-    camera.setLookTarget({0.0f,0.0f,0.0f});
-    auto pipeline = EngiGraph::DeferredPipelineOgl(500,500,camera);
-
-    auto meshes = EngiGraph::loadOBJ("meshes/mori_knob.obj");
-    auto mesh = EngiGraph::loadMeshOgl(meshes[0]);
-    auto mesh2 = EngiGraph::loadMeshOgl(meshes[1]);
-    auto mesh3 = EngiGraph::loadMeshOgl(meshes[4]);
-    auto albedo = EngiGraph::loadTextureOgl(EngiGraph::readImage("textures/test_orange.png"));
-    auto albedo2 = EngiGraph::loadTextureOgl(EngiGraph::readImage("textures/test_blue.png"));
 
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
@@ -68,85 +56,127 @@ int main(int, char**)
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
+    try {
+        EngiGraph::initOpenGl((GLADloadproc) glfwGetProcAddress);
+        EngiGraph::Camera camera{EngiGraph::AngleDegrees<float>(90.0f), 1.0f, 0.001, 1000, {0, -1, 0}};
+        camera.setPosition({5.0, 5.0, 5.0});
+        camera.setLookTarget({0.0f, 0.0f, 0.0f});
+        auto pipeline = EngiGraph::DeferredPipelineOgl(500, 500, camera);
 
-    // Our state
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-    Eigen::Vector3f cam_pos = {5.0f,5.0f,5.0f};
-    Eigen::Vector3f cam_look = {0,0,0};
+        auto meshes = EngiGraph::loadOBJ("meshes/mori_knob.obj");
+        auto mesh = EngiGraph::loadMeshOgl(meshes[0]);
+        auto mesh2 = EngiGraph::loadMeshOgl(meshes[1]);
+        auto mesh3 = EngiGraph::loadMeshOgl(meshes[4]);
+        auto albedo = EngiGraph::loadTextureOgl(EngiGraph::readImage("textures/test_orange.png"));
+        auto albedo2 = EngiGraph::loadTextureOgl(EngiGraph::readImage("textures/test_blue.png"));
 
-    int width = 500;
-    int height = 500;
-    int last_width = 500;
-    int last_height = 500;
-    // Main loop
-    while (!glfwWindowShouldClose(window))
-    {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        glfwPollEvents();
-        if(width != last_width || height != last_height){
-            last_width = width;
-            last_height = height;
-            pipeline.resize(width,height);
+
+        // Our state
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+        Eigen::Vector3f cam_pos = {5.0f, 5.0f, 5.0f};
+        Eigen::Vector3f cam_look = {0, 0, 0};
+
+        int width = 500;
+        int height = 500;
+        int last_width = 500;
+        int last_height = 500;
+        // Main loop
+        while (!glfwWindowShouldClose(window)) {
+            // Poll and handle events (inputs, window resize, etc.)
+            // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+            // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+            // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+            // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+            glfwPollEvents();
+            if (width != last_width || height != last_height) {
+                last_width = width;
+                last_height = height;
+                pipeline.resize(width, height);
+            }
+            pipeline.camera.setPosition(cam_pos);
+            pipeline.camera.setLookTarget(cam_look);
+            pipeline.submitDrawCall({mesh, albedo, Eigen::Matrix4f::Identity()});
+            pipeline.submitDrawCall({mesh2, albedo, Eigen::Matrix4f::Identity()});
+            pipeline.submitDrawCall({mesh3, albedo2, Eigen::Matrix4f::Identity()});
+
+            pipeline.render();
+
+            auto frame_buffer = pipeline.getMainFramebuffer();
+
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            if (ImGui::CollapsingHeader("Camera")) {
+                ImGui::DragFloat3("Position", cam_pos.data());
+                ImGui::DragFloat3("Target", cam_look.data());
+            }
+            if (ImGui::CollapsingHeader("World")) {
+                ImGui::ColorEdit3("Ambient", pipeline.ambient_color.data());
+            }
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+
+            ImGui::Begin("GameWindow");
+            {
+
+
+                // Using a Child allow to fill all the space of the window.
+                // It also alows customization
+                ImGui::BeginChild("GameRender");
+
+                width = ImGui::GetWindowContentRegionMax().x - ImGui::GetWindowContentRegionMin().x;
+                height = ImGui::GetWindowContentRegionMax().y - ImGui::GetWindowContentRegionMin().y;
+
+                // Get the size of the child (i.e. the whole draw size of the windows).
+                ImVec2 wsize = {(float) frame_buffer.width, (float) frame_buffer.height};
+                // Because I use the texture from OpenGL, I need to invert the V from the UV.
+                ImGui::Image((ImTextureID) frame_buffer.color_texture, wsize, ImVec2(0, 0), ImVec2(1, 1));
+                ImGui::EndChild();
+            }
+            ImGui::End();
+
+            // Rendering
+            ImGui::Render();
+            int display_w, display_h;
+            glfwGetFramebufferSize(window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w,
+                         clear_color.w);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            glfwSwapBuffers(window);
         }
-        pipeline.camera.setPosition(cam_pos);
-        pipeline.camera.setLookTarget(cam_look);
-        pipeline.submitDrawCall({mesh,albedo,Eigen::Matrix4f::Identity()});
-        pipeline.submitDrawCall({mesh2,albedo,Eigen::Matrix4f::Identity()});
-        pipeline.submitDrawCall({mesh3,albedo2,Eigen::Matrix4f::Identity()});
+    } catch (const EngiGraph::RuntimeException& e){
+        while (!glfwWindowShouldClose(window)) {
 
-        pipeline.render();
+            glfwPollEvents();
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
 
-        auto frame_buffer = pipeline.getMainFramebuffer();
+            ImGui::Begin("Error");
+            {
+                ImGui::Text("%s", e.what());
 
-                // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
+            }
+            ImGui::End();
 
-        if ( ImGui::CollapsingHeader("Camera") ) {
-            ImGui::DragFloat3("Position",cam_pos.data());
-            ImGui::DragFloat3("Target",cam_look.data());
+            // Rendering
+            ImGui::Render();
+            int display_w, display_h;
+            glfwGetFramebufferSize(window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClearColor(0.0f,0.0f,0.0f,1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+            glfwSwapBuffers(window);
         }
-        if ( ImGui::CollapsingHeader("World") ) {
-            ImGui::ColorEdit3("Ambient",pipeline.ambient_color.data());
-        }
-
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-
-        ImGui::Begin("GameWindow");
-        {
-
-
-            // Using a Child allow to fill all the space of the window.
-            // It also alows customization
-            ImGui::BeginChild("GameRender");
-
-            width = ImGui::GetWindowContentRegionMax().x - ImGui::GetWindowContentRegionMin().x;
-            height = ImGui::GetWindowContentRegionMax().y - ImGui::GetWindowContentRegionMin().y;
-
-            // Get the size of the child (i.e. the whole draw size of the windows).
-            ImVec2 wsize = {(float)frame_buffer.width,(float)frame_buffer.height};
-            // Because I use the texture from OpenGL, I need to invert the V from the UV.
-            ImGui::Image((ImTextureID)frame_buffer.color_texture, wsize, ImVec2(0, 0), ImVec2(1, 1));
-            ImGui::EndChild();
-        }
-        ImGui::End();
-
-        // Rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        glfwSwapBuffers(window);
     }
+
 
     //todo catch errors and display them as popups. Also make sure to clean up after errors.
 
