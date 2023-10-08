a0551bcf792c38a0581c4c7cf2cc86f8042504e4
PhilipPragerUrbina <72355251+PhilipPragerUrbina@users.noreply.github.com>
2023-10-08 14:28:08 -0700

    Removed library as it does not have an imporant feature

---
+++ src/Physics/Collisions/MeshCcd.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Philip on 10/8/2023.
+//
+
+#include "MeshCcd.h"
+
+namespace EngiGraph {
+} // EngiGraph
---
+++ src/Physics/Collisions/MeshCcd.h
@@ -0,0 +1,13 @@
+//
+// Created by Philip on 10/8/2023.
+//
+
+#pragma once
+
+namespace EngiGraph {
+
+    class MeshCCD {
+
+    };
+
+} // EngiGraph
